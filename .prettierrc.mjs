8b585809dfaa239d5ec5db33e2f0ea666d5e2eaf
Andreas Hell <andhell03@gmail.com>
2026-06-30 18:27:25 +0200

    Basic pages + almost complete career page

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,19 +1,19 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-    tabWidth: 4,
-    useTabs: false,
-    bracketSpacing: true,
-    singleQuote: false,
-    trailingComma: "all",
-    semi: true,
-    plugins: ["prettier-plugin-astro"],
-    overrides: [
-        {
-            files: "*.astro",
-            options: {
-                parser: "astro",
-            },
-        },
-    ],
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  singleQuote: false,
+  trailingComma: "all",
+  semi: true,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
